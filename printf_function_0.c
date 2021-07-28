1a5f6be63626003c46b1ea4ca63a7ec58024135a
sebasrengi <sebas.rengi@hotmail.com>
2021-07-28 16:20:14 -0700

    modifications_main.c

---
+++ printf_function_0.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int (*get_struct(const char *p))(va_list)
+{
+   specifier_f specifiers[] = {
+       {"i", print_int},
+       {"d", print_int},
+       {"c", print_char},
+       {"s", print_string},
+       {"%", print_port},
+       {NULL, NULL}
+   };
+    int i;
+
+    for (i = 0; specifiers[i].specifier != NULL; i++)
+    {
+        if (*(specifiers[i].specifier) == *p)
+        {
+            break;
+        }
+    }
+    return (specifiers[i].punt);
+}
