1ed079973f10b15fb49cf8b99e09c06a34579254
TheMasterTz <palavras2001@hotmail.com>
2021-07-28 14:54:56 -0700

    print function with siclos that will print the first string of characters without printing the first %.

--- _printf.c
+++ _printf.c
@@ -5,4 +5,16 @@ int _printf(const char *format, ...)
 	va_list args;
 	int (*punt)(va_list);
 	int itern, count = 0;
+
+	va_start(args, format);
+
+	for (itern = 0; format[itern] != '\0';)
+	{
+		while (format[itern] != '%' && format[itern])
+		{
+			_putchar(format[itern]);
+			count++;
+			itern++;
+		}
+	}
 }
