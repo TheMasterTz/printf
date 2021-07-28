1ab6d33e175f64e4714e3ea6beac1372d31d1fc7
TheMasterTz <palavras2001@hotmail.com>
2021-07-28 16:20:19 -0700

    adds a loop that fills the array with 0 and the number of the variadic variable

--- printf_function_1.c
+++ printf_function_1.c
@@ -6,5 +6,11 @@ int print_int(va_list args)
 	int iter, m = 1000000000, var, sum, count = 0;
 
 	var = va_arg(args, int);
+	array[0] = var / m;
 
+	for (iter = 1; iter < 10; iter++)
+	{
+		m = m / 10;
+		array[iter] = (var / m) % 10;
+	}
 }
