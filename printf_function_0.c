6f82b57be848f507746176a44c7c57f49695ae79
sebasrengi <sebas.rengi@hotmail.com>
2021-07-28 17:10:45 -0700

    betty

--- printf_function_0.c
+++ printf_function_0.c
@@ -2,27 +2,24 @@
 
 int print_port(va_list args)
 {
-    putchar('%');
-    return(1);
+putchar('%');
+return (1);
 }
-
 int print_char(va_list args)
 {
-    char ch = va_arg(args, int);
-
-    putchar(ch);
-    return (1);
+char ch = va_arg(args, int);
+putchar(ch);
+return (1);
 }
 int print_string(va_list args)
 {
-    int count;
-    char *str = va_arg(args, char *);
-
-    if (str == NULL)
-        str = "(null)";
-    for (count = 0; str[count]; count++)
-    {
-        putchar(str[count]);
-    }
-    return (count);
+int count;
+char *str = va_arg(args, char *);
+if (str == NULL)
+str = "(null)";
+for (count = 0; str[count]; count++)
+{
+putchar(str[count]);
+}
+return (count);
 }
