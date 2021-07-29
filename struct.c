7caf96b0f66c6390d4ac15f442b9f51f98fd148f
sebasrengi <sebas.rengi@hotmail.com>
2021-07-28 17:23:27 -0700

    betty3

--- struct.c
+++ struct.c
@@ -3,21 +3,20 @@
 int (*get_struct(const char *p))(va_list)
 {
 specifier_f specifiers[] = {
- {"i", print_int},
-       {"d", print_int},
-       {"c", print_char},
-       {"s", print_string},
-       {"%", print_port},
-       {NULL, NULL}
-   };
-    int i;
-
-    for (i = 0; specifiers[i].specifier != NULL; i++)
-    {
-        if (*(specifiers[i].specifier) == *p)
-        {
-            break;
-        }
-    }
-    return (specifiers[i].punt);
+{"i", print_int},
+{"d", print_int},
+{"c", print_char},
+{"s", print_string},
+{"%", print_port},
+{NULL, NULL}
+};
+int i;
+for (i = 0; specifiers[i].specifier != NULL; i++)
+{
+if (*(specifiers[i].specifier) == *p
+{
+break;
+}
+}
+return (specifiers[i].punt);
 }
